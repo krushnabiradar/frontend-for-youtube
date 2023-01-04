da5140a0d2e9480a1dedb3ebc4eef967351bd54f
krushnabiradar <krushnabiradar1995@.com>
2023-01-04 22:50:23 +0530

    Created components and completed left sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Hello there</p>
-    </div>
-  );
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./Components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
